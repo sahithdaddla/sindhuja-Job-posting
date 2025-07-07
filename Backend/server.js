e93b52cab71e99a1e229718bcd51a754ab9a0c62
sahithdaddla <daddlasahith>
2025-07-07 18:33:22 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3010;
+const port = 3206;
 
 // Middleware
 app.use(cors());
@@ -119,5 +119,5 @@ app.post('/api/jobs', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://13.53.49.44:${port}`);
+    console.log(`Server running at http://3.85.61.23:${port}`);
 });
